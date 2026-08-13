915b5ac713a2fcc04d86cdc8c8b67a6764ffb29f
Winged <winged.python@outlook.com>
2026-08-13 14:31:16 -0400

    tweaked the blossomfall white patches generation to account for the new design

--- scripts/cat/pelts.py
+++ scripts/cat/pelts.py
@@ -586,10 +586,12 @@ class Pelt:
 
                 elif KITgrade == 2:
                     #body
-                    white_pattern.append(choice(['underbelly1', 'mask n mantle', 'blossomfall']))
+                    white_pattern.append(choice(['underbelly1', 'mask n mantle']))
 
                     white_pattern.append(choice(['break/right no', 'break/left no'] + [None] * 14))
                     white_pattern.append(choice(['break/pants'] + [None] * 9))
+
+                    white_pattern.append(choice(['blossomfall'] + [None] * 15))
 
                     #paws
                     nropaws = 4
